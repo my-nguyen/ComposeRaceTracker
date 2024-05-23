e1a03945ecfe733522fcc28f37057fb8e12618b5
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1G.local>
2024-05-22 20:13:41 -0700

    B5: Write unit tests to test coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 }
